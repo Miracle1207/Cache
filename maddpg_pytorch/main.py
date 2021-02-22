8b61976f13a36cb1b054f5a3ab5bd6a37a2d8f4d
Miracle1207 <70636870+Miracle1207@users.noreply.github.com>
2021-02-22 09:24:54 +0800

    Add files via upload

--- maddpg_pytorch/main.py
+++ maddpg_pytorch/main.py
@@ -7,7 +7,7 @@ from gym.spaces import Box, Discrete
 from pathlib import Path
 from torch.autograd import Variable
 from tensorboardX import SummaryWriter
-from maddpg_pytorch.utils.make_env import make_env
+#from maddpg_pytorch.utils.make_env import make_env
 from maddpg_pytorch.utils.buffer import ReplayBuffer
 #from maddpg_pytorch.utils.env_wrappers import SubprocVecEnv, DummyVecEnv
 from maddpg_pytorch.algorithms.maddpg import MADDPG
@@ -61,6 +61,70 @@ def run(config):
                                  [acsp.shape[0] if isinstance(acsp, Box) else acsp.n
                                   for acsp in env.action_space])
     t = 0
+
+    # for ep_i in range(0, 1000, config.n_rollout_threads):
+    #     print("Episodes %i-%i of %i" % (ep_i + 1,
+    #                                     ep_i + 1 + config.n_rollout_threads,
+    #                                     config.n_episodes))
+    #     obs = env.reset()
+    #     # obs.shape = (n_rollout_threads, nagent)(nobs), nobs differs per agent so not tensor
+    #     maddpg.prep_rollouts(device='cpu')
+    #
+    #     explr_pct_remaining = max(0, config.n_exploration_eps - ep_i) / config.n_exploration_eps
+    #     maddpg.scale_noise(
+    #         config.final_noise_scale + (config.init_noise_scale - config.final_noise_scale) * explr_pct_remaining)
+    #     maddpg.reset_noise()
+    #
+    #     for et_i in range(config.episode_length):
+    #         env.render()
+    #         # rearrange observations to be per agent, and convert to torch Variable
+    #         torch_obs = [Variable(torch.Tensor(np.vstack(obs[i])), requires_grad=False) for i in
+    #                      range(env.edge_n)]  # maddpg.nagents ---3
+    #         # torch_obs = [Variable(torch.Tensor(obs[:, i]), requires_grad=False) for i in range(env.edge_n)]
+    #         # get actions as torch Variables
+    #         torch_agent_actions = replay_buffer.expert_data(torch_obs, explore=True)
+    #
+    #
+    #         # convert actions to numpy arrays
+    #         agent_actions = [ac.data.numpy() for ac in torch_agent_actions]
+    #         # expert_date_agent_actions = [ac.data.numpy() for ac in expert_data_torch_agent_actions]
+    #
+    #         # rearrange actions to be per environment
+    #         actions = [[ac[i] for ac in agent_actions] for i in range(config.n_rollout_threads)]
+    #         next_obs, rewards, dones, infos = env.step(actions)
+    #
+    #         replay_buffer.push(obs, agent_actions, rewards, next_obs, dones)
+    #         obs = next_obs
+    #         t += config.n_rollout_threads
+    #         if (len(replay_buffer) >= config.batch_size and
+    #                 (t % config.steps_per_update) < config.n_rollout_threads):
+    #             if USE_CUDA:
+    #                 maddpg.prep_training(device='gpu')
+    #             else:
+    #                 maddpg.prep_training(device='cpu')
+    #             for u_i in range(config.n_rollout_threads):
+    #                 for a_i in range(maddpg.nagents):
+    #                     sample = replay_buffer.sample(config.batch_size,
+    #                                                   to_gpu=USE_CUDA)
+    #                     maddpg.update(sample, a_i, logger=logger)
+    #                 maddpg.update_all_targets()
+    #             maddpg.prep_rollouts(device='cpu')
+    #     ep_rews = replay_buffer.get_average_rewards(
+    #         config.episode_length * config.n_rollout_threads)
+    #     # ep_rews = replay_buffer.get_average_rewards(
+    #     #     50)
+    #     for a_i, a_ep_rew in enumerate(ep_rews):
+    #         # print('agent%i/mean_episode_rewards' % a_i, a_ep_rew, ep_i)
+    #         logger.add_scalar('agent%i/mean_episode_rewards' % a_i, a_ep_rew, ep_i)
+    #     logger.add_scalar('total_mean_episode_rewards', sum(ep_rews), ep_i)
+    #     print('total_mean_episode_rewards', sum(ep_rews), ep_i)
+    #
+    #     if ep_i % config.save_interval < config.n_rollout_threads:
+    #         os.makedirs(run_dir / 'incremental', exist_ok=True)
+    #         maddpg.save(run_dir / 'incremental' / ('model_ep%i.pt' % (ep_i + 1)))
+    #         maddpg.save(run_dir / 'model.pt')
+
+
     for ep_i in range(0, config.n_episodes, config.n_rollout_threads):
         print("Episodes %i-%i of %i" % (ep_i + 1,
                                         ep_i + 1 + config.n_rollout_threads,
@@ -80,24 +144,14 @@ def run(config):
             #torch_obs = [Variable(torch.Tensor(obs[:, i]), requires_grad=False) for i in range(env.edge_n)]
             # get actions as torch Variables
             torch_agent_actions = maddpg.step(torch_obs, explore=True)
+            # expert_data_torch_agent_actions = replay_buffer.expert_data(torch_obs, explore=True)
+
             # convert actions to numpy arrays
             agent_actions = [ac.data.numpy() for ac in torch_agent_actions]
+            # expert_date_agent_actions = [ac.data.numpy() for ac in expert_data_torch_agent_actions]
+
             # rearrange actions to be per environment
             actions = [[ac[i] for ac in agent_actions] for i in range(config.n_rollout_threads)]
-            '''
-            env_wrapper
-            '''
-            def action_wrapper(acs):
-                acs = acs[0]
-                ac_str = []
-                ac = np.zeros(shape=(len(acs), 5))
-                for i in range(len(acs)):
-                    ac_str.append(bin(list(acs[i]).index(1)).lstrip('0b'))
-                    for j in range(len(ac_str[i])):
-                        ac[i][4-j] = int(ac_str[i][len(ac_str[i])-1-j])
-                return ac
-            actions = action_wrapper(actions)
-
             next_obs, rewards, dones, infos = env.step(actions)
 
             replay_buffer.push(obs, agent_actions, rewards, next_obs, dones)
@@ -118,6 +172,8 @@ def run(config):
                 maddpg.prep_rollouts(device='cpu')
         ep_rews = replay_buffer.get_average_rewards(
             config.episode_length * config.n_rollout_threads)
+        # ep_rews = replay_buffer.get_average_rewards(
+        #     50)
         for a_i, a_ep_rew in enumerate(ep_rews):
             #print('agent%i/mean_episode_rewards' % a_i, a_ep_rew, ep_i)
             logger.add_scalar('agent%i/mean_episode_rewards' % a_i, a_ep_rew, ep_i)
@@ -137,7 +193,7 @@ def run(config):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("--env_id", help="Name of environment", default="cache_data")
+    parser.add_argument("--env_id", help="Name of environment", default="20210204_data")
     parser.add_argument("--model_name",
                         help="Name of directory to store " +
                              "model/training contents", default="./model")
@@ -146,9 +202,9 @@ if __name__ == '__main__':
                         help="Random seed")
     parser.add_argument("--n_rollout_threads", default=1, type=int) # walker
     parser.add_argument("--n_training_threads", default=6, type=int)
-    parser.add_argument("--buffer_length", default=int(1e6), type=int)
-    parser.add_argument("--n_episodes", default=25000, type=int)
-    parser.add_argument("--episode_length", default=25, type=int) # TODO increase the lenghth 25
+    parser.add_argument("--buffer_length", default=int(1e5), type=int)
+    parser.add_argument("--n_episodes", default=5000, type=int) # 10000 better
+    parser.add_argument("--episode_length", default=100, type=int) # TODO increase the lenghth 25
     parser.add_argument("--steps_per_update", default=100, type=int)
     parser.add_argument("--batch_size",
                         default=1024, type=int,
